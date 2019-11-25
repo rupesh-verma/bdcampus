064786e60eee5adeac10cef9696535c468d8f8c9
Rupesh <sanjeev@diarsotechnologies.com>
2019-11-25 13:05:04 +0530

    k

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC1uRgghZBxxqeRnCLl3fY1OBipy1mbhyE",
+    authDomain: "bd-campus.firebaseapp.com",
+    databaseURL: "https://bd-campus.firebaseio.com",
+    projectId: "bd-campus",
+    storageBucket: "bd-campus.appspot.com",
+    messagingSenderId: "470595948114",
+    appId: "1:470595948114:web:90cf6d395154c6a9b7198b",
+    measurementId: "G-PGZMVGJDN3"
+  },
 };
